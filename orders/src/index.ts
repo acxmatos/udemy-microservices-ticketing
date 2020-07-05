c303b09a993e7ca51f7e9d47892534b850fa2ab0
Antonio Matos <acxmatos@gmail.com>
2020-07-05 18:31:53 -0300

    updated start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -10,7 +10,7 @@ import { PaymentCreatedListener } from "./events/listeners/payment-created-liste
 // Created a separated function to use async/await in old versions of Node
 // New versions allow to use await on a top level (no need to use a function)
 const start = async () => {
-  console.log("Starting up...");
+  console.log("Starting up....");
   
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
